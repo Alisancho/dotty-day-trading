cb397115a264763638af5e540d46da592326320f
alisancho <sashabig4328@protonmail.com>
2020-07-13 22:31:48 +0300

    start

--- src/main/scala/ru/deytrading/Main.scala
+++ src/main/scala/ru/deytrading/Main.scala
@@ -1,4 +1,27 @@
 package ru.deytrading
 
-object Main extends App {
+import java.util.logging.Logger
+
+import akka.actor.ActorSystem
+import akka.stream.{ActorMaterializer, Materializer}
+import cats.effect.{ExitCode, IO, IOApp}
+import ru.tinkoff.invest.openapi.OpenApi
+import ru.tinkoff.invest.openapi.okhttp.OkHttpOpenApiFactory
+
+import scala.concurrent.ExecutionContextExecutor
+
+object Main extends IOApp {
+  implicit val system: ActorSystem                = ActorSystem()
+  implicit val ec: ExecutionContextExecutor       = system.dispatcher
+  implicit val materializer: Materializer         = ActorMaterializer()
+  
+  def run(args: List[String]): IO[ExitCode] = for {
+    _ <- IO.unit
+  } yield ExitCode(1)
+
+
+  private val apiTask: IO[OpenApi] = for {
+    log <- IO {Logger.getLogger("Pooo")}
+    api <- IO {new OkHttpOpenApiFactory(TOKEN, log).createOpenApiClient(ec)}
+  } yield api
 }
